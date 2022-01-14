d83817c248abb4249ee3a28a66bd1c0e9200c74b
Brian Bycynski <high5bri@hotmail.com>
2022-01-14 06:26:21 -0500

    Light & Dark toggle

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //
 //
 
-console.log(123);
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+console.log(toggleSwitch);
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+  // console.log(event.target.checked);
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    // document.body.style.color = '#fff';
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
